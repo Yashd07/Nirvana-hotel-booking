98c3625b3ea9229fa101e2df4df9eca49448995c
Yashd07 <yashd1272@gmail.com>
2024-08-25 20:04:06 +0530

    changes done in frontend

---
+++ frontend/src/component/common/Footer.jsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+                Nirvana Hotel | All Right Reserved &copy; {new Date().getFullYear()} | Made by Yash Desai  💜
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
